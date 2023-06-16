2ebfe0d47036cd780324e74d971bfcfdcab1b0a9
Your Name <you@example.com>
2023-06-16 20:58:57 +0600

    f

--- public/main.js
+++ public/main.js
@@ -1,9 +1,22 @@
-let shadow = document.getElementById("shadowBack")
-let modal = document.getElementById("modalForm")
-let delete_button = document.getElementById("table_delete")
+let shadow = document.getElementById("shadowBack");
+let modal = document.getElementById("modalForm");
+let delete_button = document.getElementById("table_delete");
+let edit = document.getElementById('table_edit');
+let col_info = document.querySelector('.col.name h3').innerHTML
+let lic_info = document.querySelector('.col.lic h3').innerHTML
+let track_info = document.querySelector('.col.track h3').innerHTML
+
+let table_info = [{
+  title: col_info,
+  lic: lic_info,
+  track: track_info
+
+}]
+console.log(table_info)
+
 const modalOpen = () => {
-    modalUpdate();
-    modal.innerHTML = `
+  modalUpdate();
+  modal.innerHTML = `
     <div class="top">
               <h3>Добавить тариф</h3>
               <input type="text" id="tariff_title" placeholder="Название тарифа" />
@@ -17,88 +30,84 @@ const modalOpen = () => {
               <input type="button" value="Отменить" class="btn dark" onclick="modalClose()" />
               <input type="button" value="Добавить" class="btn main" onclick="addTariff()" />
             </div>
-    `
-    let lic = document.getElementById("tariff_lic")
-    let track = document.getElementById("tariff_track")
-    lic.addEventListener('input', calculatePrice)
-    track.addEventListener('input', calculatePrice)
-   
-   
-   
-    modal.classList.add("active");
-    shadow.classList.add("active")
-}
+    `;
+  let lic = document.getElementById("tariff_lic");
+  let track = document.getElementById("tariff_track");
+  lic.addEventListener("input", calculatePrice);
+  track.addEventListener("input", calculatePrice);
+  modal.classList.add("active");
+  shadow.classList.add("active");
+};
 const calculatePrice = () => {
-  let modalPrice = document.querySelector('.price p')
-  let lic = document.getElementById("tariff_lic")
-  let track = document.getElementById("tariff_track")
-  let licValue = parseFloat(lic.value)
-  let trackValue = parseFloat(track.value)
-  let count
+  let modalPrice = document.querySelector(".price p");
+  let lic = document.getElementById("tariff_lic");
+  let track = document.getElementById("tariff_track");
+
+  let licValue = parseFloat(lic.value);
+  let trackValue = parseFloat(track.value);
+  let count;
   if (!isNaN(licValue) && !isNaN(trackValue)) {
     if (licValue < 3 && licValue > 0) {
-      count = (trackValue * 2000) + "₸";
+      count = trackValue * 2000 + "₸";
       modalPrice.innerHTML = count;
     } else if (licValue >= 3 && licValue < 5) {
-      count = (trackValue * 1800) + "₸";
+      count = trackValue * 1800 + "₸";
       modalPrice.innerHTML = count;
     } else if (licValue >= 5) {
-      count = (trackValue * 1680) + "₸";
+      count = trackValue * 1680 + "₸";
       modalPrice.innerHTML = count;
     }
   } else {
     count = "0₸";
     modalPrice.innerHTML = count;
   }
-}
+};
 const modalClose = () => {
-  modal.classList.remove("active")
-  shadow.classList.remove("active")
-  delete_button.classList.remove('active')
-}
-
+  modal.classList.remove("active");
+  shadow.classList.remove("active");
+  delete_button.classList.remove("active");
+  edit.classList.remove('active')
+};
 
 const modalUpdate = () => {
-    modal.innerHTML = '';
-}
+  modal.innerHTML = "";
+};
 function validateInput(input) {
-  input.value = input.value.replace(/\D/g, '');
+  input.value = input.value.replace(/\D/g, "");
 }
-
-
 
 const addTariff = () => {
   let lic = document.getElementById("tariff_lic");
   let track = document.getElementById("tariff_track");
   let tariff_title = document.getElementById("tariff_title").value;
-  let modalPrice = document.querySelector('.price p');
+  let modalPrice = document.querySelector(".price p");
   let licValue = parseFloat(lic.value);
   let trackValue = parseFloat(track.value);
   let count;
 
   if (licValue && trackValue && tariff_title) {
     if (licValue < 3 && licValue > 0) {
-      count = (trackValue * 2000) + "₸";
+      count = trackValue * 2000 + "₸";
       modalPrice.innerHTML = count;
     } else if (licValue >= 3 && licValue < 5) {
-      count = (trackValue * 1800) + "₸";
+      count = trackValue * 1800 + "₸";
       modalPrice.innerHTML = count;
     } else if (licValue >= 5) {
-      count = (trackValue * 1680) + "₸";
+      count = trackValue * 1680 + "₸";
       modalPrice.innerHTML = count;
     }
-
-    let table = document.querySelector('.table-container');
+    let table = document.querySelector(".table-container");
+    
     let table_tariff = [
       {
         title: tariff_title,
         licenses: licValue,
         tracks: trackValue,
-        price: count
-      }
+        price: count,
+      },
     ];
 
-    table_tariff.forEach(item => {
+    table_tariff.forEach((item) => {
       let new_row = `
         <div class="row">
           <div class="col name"><h3>${item.title}</h3></div>
@@ -106,7 +115,7 @@ const addTariff = () => {
           <div class="col"><h3>${item.tracks}</h3></div>
           <div class="col"><h3>${item.price}</h3></div>
           <div class="col bd-r action">
-            <div class="square pen">
+            <div class="square pen" onclick="openEdit(this)">
               <svg width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M14.2038 7.79595L15.9998 5.99994C16.545 5.45469 16.8176 5.18207 16.9634 4.88797C17.2407 4.32842 17.2407 3.67146 16.9634 3.11191C16.8176 2.81782 16.545 2.54519 15.9998 1.99994C15.4545 1.45469 15.1819 1.18207 14.8878 1.03633C14.3282 0.759048 13.6713 0.759048 13.1117 1.03633C12.8176 1.18207 12.545 1.45469 11.9998 1.99994L10.1811 3.8186C11.145 5.4692 12.5311 6.84476 14.2038 7.79595ZM8.72666 5.27305L1.85615 12.1436C1.43109 12.5686 1.21856 12.7812 1.07883 13.0422C0.939091 13.3033 0.880146 13.5981 0.762256 14.1875L0.14686 17.2645C0.0803376 17.5971 0.0470765 17.7634 0.141684 17.858C0.236293 17.9526 0.402598 17.9194 0.735208 17.8529L3.81219 17.2375C4.40164 17.1196 4.69637 17.0606 4.95746 16.9209C5.21856 16.7812 5.43109 16.5686 5.85615 16.1436L12.7456 9.25416C11.1239 8.23851 9.7522 6.87622 8.72666 5.27305Z" />
               </svg>
@@ -123,16 +132,18 @@ const addTariff = () => {
 
       table.innerHTML += new_row;
     });
-    
-    modal.classList.remove('active');
-    shadow.classList.remove('active');
+
+    modal.classList.remove("active");
+    shadow.classList.remove("active");
   }
-  
 };
 
+let tempDelete;
 
 const tableDelete = (element) => {
-  let col_name = element.parentNode.parentNode.querySelector('.col.name h3').innerHTML;
+  tempDelete = element.parentNode.parentNode;
+  let col_name =
+    element.parentNode.parentNode.querySelector(".col.name h3").innerHTML;
   delete_button.innerHTML = `
     <div class="top">
       <h3>Удалить тариф ${col_name}</h3>
@@ -142,15 +153,38 @@ const tableDelete = (element) => {
       <input type="button" value="Удалить" class="btn main" onclick="deleteRow(this)" />
     </div>
   `;
-  
   console.log(col_name);
-  
-  delete_button.classList.add('active');
-  shadow.classList.add('active');
+
+  delete_button.classList.add("active");
+  shadow.classList.add("active");
 };
 const deleteRow = (e) => {
-  const row = e.parentNode.parentNode.parentNode.querySelector('.row');
-  row.remove()
-  console.log(row)
+  tempDelete.remove();
+  modalClose();
+};
 
+const openEdit = (element) => {
+  let col_name =
+    element.parentNode.parentNode.querySelector(".col.name h3").innerHTML;
+  modalUpdate();
+  edit.innerHTML = `
+    <div class="top">
+              <h3>Тариф "${col_name}"</h3>
+              <input type="text" id="tariff_title" placeholder="Название тарифа" />
+              <input type="text" id="tariff_lic" oninput="validateInput(this)" />
+              <input type="text" id="tariff_track" oninput="validateInput(this)" />
+              <div class="price">
+                <p class="BodyM">0₸</p>
+              </div>
+            </div>
+            <div class="bot">
+              <input type="button" value="Закрыть" class="btn dark" onclick="modalClose()" />
+              <input type="button" value="Изменить" class="btn main" onclick="addTariff()" />
+            </div>
+    `;
+
+
+
+  edit.classList.add('active')
+  shadow.classList.add('active')
 }
